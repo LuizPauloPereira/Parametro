14a932c48da58c175f08d54795e5ce7d255f87e2
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-03-28 21:08:33 -0400

    passando string de link atraves de ViewBag

--- parametro/Controllers/ProdutoController.cs
+++ parametro/Controllers/ProdutoController.cs
@@ -11,22 +11,26 @@ namespace parametro.Controllers
         // GET: Produto
         public ActionResult Buscar(int prd)
         {
-
+            string link = string.Empty;
             string retorno = string.Empty;
             switch (prd)
             {
                 case 1:
                     retorno = "SAMSUNG GALAXY NOTE 3";
+                    link = "https://www.tudocelular.com/Samsung/fichas-tecnicas/n2503/Samsung-Galaxy-Note-3.html";
                     break;
                 case 5:
                     retorno = "IPHONE 8";
+                    link = "https://www.tudocelular.com/Apple/fichas-tecnicas/n3501/Apple-iPhone-8.html"; 
                     break;
                 case 10:
                     retorno = "NOKIA 1208";
+                    link = "https://www.tudocelular.com/Nokia/fichas-tecnicas/n1235/Nokia-1208.html";
                     break;
             }
 
             ViewBag.Produto = retorno;
+            ViewBag.Link = link;
             return View();
         }
     }
