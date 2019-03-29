0b7bb967a3c759cf8c43c27bf0a62f2b496e2ba7
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-03-28 20:30:35 -0400

    inicial, criando HomeController, e a sua view Index, e a ProdutoController e a view Buscar

---
+++ parametro/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace parametro.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Buscar(int prd)
+        {
+
+            string retorno = string.Empty;
+            switch (prd)
+            {
+                case 1:
+                    retorno = "SAMSUNG GALAXY NOTE 3";
+                    break;
+                case 5:
+                    retorno = "IPHONE 8";
+                    break;
+                case 10:
+                    retorno = "NOKIA 1208";
+                    break;
+            }
+
+            ViewBag.Produto = retorno;
+            return View();
+        }
+    }
+}
